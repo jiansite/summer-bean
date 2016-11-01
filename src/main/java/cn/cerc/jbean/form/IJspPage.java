b16759f64f6ac6065fdb94676f0f1c95489f3bfa
张弓 <sz9214e@qq.com>
2016-11-01 11:32:06 +0800

    加入默认的函数getValue

--- src/main/java/cn/cerc/jbean/form/IJspPage.java
+++ src/main/java/cn/cerc/jbean/form/IJspPage.java
@@ -1,5 +1,8 @@
 package cn.cerc.jbean.form;
 
+import cn.cerc.jbean.other.MemoryBuffer;
+import cn.cerc.jdb.other.utils;
+
 public interface IJspPage extends IPage {
 	// 输出到支持Jstl的jsp文件
 	public void add(String id, Object value);
@@ -12,4 +15,23 @@ public interface IJspPage extends IPage {
 
 	// 返回带设备码的jsp文件
 	public String getViewFile();
+	
+
+	// 从请求或缓存读取数据
+	default public String getValue(MemoryBuffer buff, String reqKey) {
+		String result = getRequest().getParameter(reqKey);
+		if (result == null) {
+			String val = buff.getString(reqKey).replace("{}", "");
+			if (utils.isNumeric(val) && val.endsWith(".0"))
+				result = val.substring(0, val.length() - 2);
+			else
+				result = val;
+		} else {
+			result = result.trim();
+			buff.setField(reqKey, result);
+		}
+		this.add(reqKey, result);
+		return result;
+	}
+
 }
