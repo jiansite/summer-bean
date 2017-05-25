70e663b38e671bcf6bb64cd31445bd06a06309f0
李远 <460289628@qq.com>
2017-05-25 10:28:46 +0800

    增加系统参数，网单代发货利润率（需启用O2O模组），默认值0.6

--- src/main/java/cn/cerc/jbean/other/BookOptions.java
+++ src/main/java/cn/cerc/jbean/other/BookOptions.java
@@ -94,6 +94,7 @@ public class BookOptions {
 	public static final String BEDefaultBankAccount = "BEDefaultBankAccount";
 	public static final String UpdateTBDateToEffectiveDate = "UpdateTBDateToEffectiveDate";
 	public static final String IsViewOldMenu = "IsViewOldMenu";
+	public static final String DefaultProfitMargin = "DefaultProfitMargin";
 
 	private static Map<String, String> items = new HashMap<>();
 
@@ -180,6 +181,7 @@ public class BookOptions {
 		items.put(UpdateCurrentMonthProfit, "在修改进货价后，每晚自动更新本月所有单据的成本价与毛利");
 		items.put(UpdateTBDateToEffectiveDate, "单据生效时，单据日期自动等于生效日期");
 		items.put(IsViewOldMenu, "是否显示旧版菜单链接（带闪电标识）");
+		items.put(DefaultProfitMargin, "网单代发货利润率（<font color=red>需启用O2O模组</font>）");
 
 		// 安全管控参数
 		items.put(EnableReportSecurity, "启用安全报表控制，未确认的单据不允许打印报表");
