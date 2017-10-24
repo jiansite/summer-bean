c5954a4e0aa1e25d09edba2977d38d29fb7ed147
韦善贵 <873261248@qq.com>
2017-10-24 18:03:11 +0800

    [feature] 增加内容排列方式属性

--- src/main/java/cn/cerc/jexport/excel/Column.java
+++ src/main/java/cn/cerc/jexport/excel/Column.java
@@ -3,54 +3,65 @@ package cn.cerc.jexport.excel;
 import cn.cerc.jdb.core.Record;
 
 public abstract class Column {
-    // 对应数据集字段名
-    private String code;
-    // 对应数据集字段标题
-    private String name;
-    // 列宽度
-    private int width;
-    // 数据源
-    private Record record;
+	// 对应数据集字段名
+	private String code;
+	// 对应数据集字段标题
+	private String name;
+	// 列宽度
+	private int width;
+	// 排列方式
+	private String align;
+	// 数据源
+	private Record record;
 
-    // 取得数据
-    public abstract Object getValue();
+	// 取得数据
+	public abstract Object getValue();
 
-    public String getString() {
-        return record.getString(code);
-    }
+	public String getString() {
+		return record.getString(code);
+	}
 
-    public String getCode() {
-        return code;
-    }
+	public String getCode() {
+		return code;
+	}
 
-    public Column setCode(String code) {
-        this.code = code;
-        return this;
-    }
+	public Column setCode(String code) {
+		this.code = code;
+		return this;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public Column setName(String name) {
-        this.name = name;
-        return this;
-    }
+	public Column setName(String name) {
+		this.name = name;
+		return this;
+	}
 
-    public Record getRecord() {
-        return record;
-    }
+	public Record getRecord() {
+		return record;
+	}
 
-    public void setRecord(Record record) {
-        this.record = record;
-    }
+	public void setRecord(Record record) {
+		this.record = record;
+	}
 
-    public int getWidth() {
-        return width;
-    }
+	public int getWidth() {
+		return width;
+	}
 
-    public Column setWidth(int width) {
-        this.width = width;
-        return this;
-    }
+	public Column setWidth(int width) {
+		this.width = width;
+		return this;
+	}
+
+	public String getAlign() {
+		return align;
+	}
+
+	public Column setAlign(String align) {
+		this.align = align;
+		return this;
+	}
 }
