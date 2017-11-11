9685cd7bdd7355bbbc90ae183d88e1b6ecfd3dda
张弓 <sz9214e@qq.com>
2017-11-11 08:01:53 +0800

    代码格式重排，以及统一调用接口

--- src/main/java/cn/cerc/jbean/core/ServerConfig.java
+++ src/main/java/cn/cerc/jbean/core/ServerConfig.java
@@ -12,150 +12,168 @@ import org.apache.log4j.Logger;
 import cn.cerc.jdb.core.IConfig;
 
 public class ServerConfig implements IConfig {
-    private static final Logger log = Logger.getLogger(ServerConfig.class);
-    private static Properties properties = new Properties();
-    private Map<String, String> defaultParams = new HashMap<>();
+	public static final String TaskServiceEnabled = "task.service";
+	public static final String TaskServiceToken = "task.token";
+	public static final String AdminMobile = "admin.mobile";
+	public static final String AdminEmail = "admin.email";
+	
+	private static final Logger log = Logger.getLogger(ServerConfig.class);
+	private static ServerConfig instance;
+	private static Properties properties = new Properties();
+	private Map<String, String> defaultParams = new HashMap<>();
 
-    public static final int appNone = 0;
-    public static final int appTest = 1;
-    public static final int appBeta = 2;
-    public static final int appRelease = 3;
+	public static final int appNone = 0;
+	public static final int appTest = 1;
+	public static final int appBeta = 2;
+	public static final int appRelease = 3;
 
-    static {
-        String confFile = "/application.properties";
-        try {
-            InputStream file = ServerConfig.class.getResourceAsStream(confFile);
-            if (file != null) {
-                properties.load(file);
-                log.info("read from file: " + confFile);
-            } else {
-                log.error("not find file: " + confFile);
-            }
-        } catch (FileNotFoundException e) {
-            log.error("The settings file '" + confFile + "' does not exist.");
-        } catch (IOException e) {
-            log.error("Failed to load the settings from the file: " + confFile);
-        }
-    }
+	static {
+		String confFile = "/application.properties";
+		try {
+			InputStream file = ServerConfig.class.getResourceAsStream(confFile);
+			if (file != null) {
+				properties.load(file);
+				log.info("read from file: " + confFile);
+			} else {
+				log.error("not find file: " + confFile);
+			}
+		} catch (FileNotFoundException e) {
+			log.error("The settings file '" + confFile + "' does not exist.");
+		} catch (IOException e) {
+			log.error("Failed to load the settings from the file: " + confFile);
+		}
+	}
 
-    public static String getAppName() {
-        String result = getPropertyValue("appName", "localhost");
-        return result;
-    }
+	public static ServerConfig getInstance() {
+		if (instance == null)
+			instance = new ServerConfig();
+		return instance;
+	}
 
-    public static int getAppLevel() {
-        String tmp = getPropertyValue("version", "beta");
-        if ("test".equals(tmp))
-            return 1;
-        if ("beta".equals(tmp))
-            return 2;
-        if ("release".equals(tmp))
-            return 3;
-        else
-            return 0;
-    }
+	public static String getAppName() {
+		String result = getInstance().getProperty("appName", "localhost");
+		return result;
+	}
 
-    public static int getTimeoutWarn() {
-        String str = getPropertyValue("timeout.warn", "60");
-        return Integer.parseInt(str); // 默认60秒
-    }
+	public static int getAppLevel() {
+		String tmp = getInstance().getProperty("version", "beta");
+		if ("test".equals(tmp))
+			return 1;
+		if ("beta".equals(tmp))
+			return 2;
+		if ("release".equals(tmp))
+			return 3;
+		else
+			return 0;
+	}
 
-    public static String getAdminMobile() {
-        return getPropertyValue("admin.mobile", null);
-    }
+	@Deprecated
+	public static int getTimeoutWarn() {
+		String str = getInstance().getProperty("timeout.warn", "60");
+		return Integer.parseInt(str); // 默认60秒
+	}
 
-    public static String getAdminEmail() {
-        return getPropertyValue("admin.email", null);
-    }
+	public static String getAdminMobile() {
+		return getInstance().getProperty(AdminMobile, null);
+	}
 
-    // 日志服务
-    public static String ots_endPoint() {
-        return getPropertyValue("ots.endPoint", null);
-    }
+	public static String getAdminEmail() {
+		return getInstance().getProperty(AdminEmail, null);
+	}
 
-    public static String ots_accessId() {
-        return getPropertyValue("ots.accessId", null);
-    }
+	// 日志服务
+	@Deprecated
+	public static String ots_endPoint() {
+		return getInstance().getProperty("ots.endPoint", null);
+	}
 
-    public static String ots_accessKey() {
-        return getPropertyValue("ots.accessKey", null);
-    }
+	@Deprecated
+	public static String ots_accessId() {
+		return getInstance().getProperty("ots.accessId", null);
+	}
 
-    public static String ots_instanceName() {
-        return getPropertyValue("ots.instanceName", null);
-    }
+	@Deprecated
+	public static String ots_accessKey() {
+		return getInstance().getProperty("ots.accessKey", null);
+	}
 
-    // 简讯服务(旧版本)
-    public static String sms_host() {
-        return getPropertyValue("sms.host", null);
-    }
+	@Deprecated
+	public static String ots_instanceName() {
+		return getInstance().getProperty("ots.instanceName", null);
+	}
 
-    public static String sms_username() {
-        return getPropertyValue("sms.username", null);
-    }
+	// 简讯服务(旧版本)
+	@Deprecated
+	public static String sms_host() {
+		return getInstance().getProperty("sms.host", null);
+	}
 
-    public static String sms_password() {
-        return getPropertyValue("sms.password", null);
-    }
+	@Deprecated
+	public static String sms_username() {
+		return getInstance().getProperty("sms.username", null);
+	}
 
-    // 微信服务
-    public static String wx_host() {
-        return getPropertyValue("wx.host", null);
-    }
+	@Deprecated
+	public static String sms_password() {
+		return getInstance().getProperty("sms.password", null);
+	}
 
-    public static String wx_appid() {
-        return getPropertyValue("wx.appid", null);
-    }
+	// 微信服务
+	public static String wx_host() {
+		return getInstance().getProperty("wx.host", null);
+	}
 
-    public static String wx_secret() {
-        return getPropertyValue("wx.secret", null);
-    }
+	public static String wx_appid() {
+		return getInstance().getProperty("wx.appid", null);
+	}
 
-    public static String dayu_serverUrl() {
-        return getPropertyValue("dayu.serverUrl", null);
-    }
+	public static String wx_secret() {
+		return getInstance().getProperty("wx.secret", null);
+	}
 
-    public static String dayu_appKey() {
-        return getPropertyValue("dayu.appKey", null);
-    }
+	@Deprecated
+	public static String dayu_serverUrl() {
+		return getInstance().getProperty("dayu.serverUrl", null);
+	}
 
-    public static String dayu_appSecret() {
-        return getPropertyValue("dayu.appSecret", null);
-    }
+	@Deprecated
+	public static String dayu_appKey() {
+		return getInstance().getProperty("dayu.appKey", null);
+	}
 
-    @Override
-    public String getProperty(String key, String def) {
-        String result = null;
-        if (properties != null)
-            result = properties.getProperty(key);
-        return result != null ? result : def;
-    }
+	@Deprecated
+	public static String dayu_appSecret() {
+		return getInstance().getProperty("dayu.appSecret", null);
+	}
 
-    private static String getPropertyValue(String key, String def) {
-        String result = null;
-        if (properties != null)
-            result = properties.getProperty(key);
-        return result != null ? result : def;
-    }
+	public static String getTaskToken() {
+		return getInstance().getProperty(TaskServiceToken, null);
+	}
 
-    public static String getTaskToken() {
-        return getPropertyValue("task.token", null);
-    }
+	public static boolean enableTaskService() {
+		return "1".equals(getInstance().getProperty(TaskServiceEnabled, null));
+	}
 
-    public static boolean enableTaskService() {
-        return "1".equals(getPropertyValue("task.service", null));
-    }
+	@Deprecated
+	public Map<String, String> getDefaultParams() {
+		return defaultParams;
+	}
 
-    public Map<String, String> getDefaultParams() {
-        return defaultParams;
-    }
+	@Deprecated
+	public void setDefaultParams(Map<String, String> defaultParams) {
+		this.defaultParams = defaultParams;
+	}
 
-    public void setDefaultParams(Map<String, String> defaultParams) {
-        this.defaultParams = defaultParams;
-    }
+	@Override
+	public String getProperty(String key, String def) {
+		String result = null;
+		if (properties != null)
+			result = properties.getProperty(key);
+		return result != null ? result : def;
+	}
 
-    @Override
-    public String getProperty(String key) {
-        return getProperty(key, null);
-    }
+	@Override
+	public String getProperty(String key) {
+		return getProperty(key, null);
+	}
 }
