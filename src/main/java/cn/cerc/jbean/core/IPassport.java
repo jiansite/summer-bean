c87e18107183aa339dd354125c3c189f4957209d
黄荣君 <l1091462907@gmail.com>
2017-12-20 17:19:41 +0800

    增加专用的form权限判断函数

--- src/main/java/cn/cerc/jbean/core/IPassport.java
+++ src/main/java/cn/cerc/jbean/core/IPassport.java
@@ -1,8 +1,20 @@
 package cn.cerc.jbean.core;
 
+import cn.cerc.jbean.form.IForm;
 import cn.cerc.jbean.rds.PassportRecord;
 
 public interface IPassport {
+
+    // 是否有菜单的执行权限
+    default boolean passForm(IForm form) {
+        String securityCheck = form.getParam("security", "true");
+        if (!"true".equals(securityCheck)) {
+            return true;
+        }
+        String verList = form.getParam("verlist", null);
+        String procCode = form.getPermission();
+        return passProc(verList, procCode);
+    }
 
     // 是否有程序的执行权限
     boolean passProc(String versions, String procCode);
